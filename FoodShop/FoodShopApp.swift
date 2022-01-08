13a10e5bd4d8ca274a725e852f13650e8f84678d
Maxmudov13 <maxmudovilyos2003@gmail.com>
2022-01-09 04:06:26 +0500

    Initial Commit

---
+++ FoodShop/FoodShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodShopApp.swift
+//  FoodShop
+//
+//  Created by user on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
